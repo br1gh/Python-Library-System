a83d4417b59e21c28789c42ed5172d8a7b604ebd
Karol Długoń <k.dlugon@appgo.pl>
2022-05-30 15:50:33 +0200

    Inserts and tab for authors

--- Files/Database.py
+++ Files/Database.py
@@ -1,58 +1,72 @@
 import sqlite3
+import os.path
 
 
 def create():
+    if not os.path.exists('library.db'):
+        con = sqlite3.connect('library.db')
+        cur = con.cursor()
+
+        cur.execute('''CREATE TABLE readers(
+            id INTEGER PRIMARY KEY autoincrement,
+            email varchar,
+            login varchar,
+            password varchar,
+            first_name varchar,
+            last_name varchar,
+            birth_date date,
+            city varchar,
+            street varchar,
+            house_number varchar,
+            phone varchar)''')
+
+        cur.execute('''CREATE TABLE authors(
+            id INTEGER PRIMARY KEY autoincrement,
+            first_name varchar,
+            last_name varchar,
+            birth_date date)''')
+
+        cur.execute('''CREATE TABLE genres(
+            id INTEGER PRIMARY KEY autoincrement,
+            name varchar)''')
+
+        cur.execute('''CREATE TABLE books(
+            id INTEGER PRIMARY KEY autoincrement,
+            id_author int,
+            id_genre int,
+            title varchar,
+            page_number int)''')
+
+        cur.execute('''CREATE TABLE copy(
+            id INTEGER PRIMARY KEY autoincrement,
+            id_book int,
+            available boolean)''')
+
+        cur.execute('''CREATE TABLE borrows(
+             id INTEGER PRIMARY KEY autoincrement,
+             id_reader int REFERENCES readers(id),
+             id_copy int REFERENCES copy(id),
+             borrow_date date,
+             return_date date)''')
+
+        con.commit()
+        print('Database created')
+        con.close()
+
+    else:
+        print('Database already exists')
+
+def insert(arr, values):
     con = sqlite3.connect('library.db')
     cur = con.cursor()
-
-    cur.execute('''CREATE TABLE readers(
-        id int primary key, 
-        email varchar,
-        login varchar,
-        password varchar,
-        first_name varchar,
-        last_name varchar,
-        birth_date date,
-        city varchar,
-        street varchar,
-        house_number varchar,
-        phone varchar)''')
-
-    cur.execute('''CREATE TABLE authors(
-        id int primary key, 
-        first_name varchar,
-        last_name varchar,
-        birth_date date)''')
-
-    cur.execute('''CREATE TABLE genres(
-        id int primary key, 
-        name varchar)''')
-
-    cur.execute('''CREATE TABLE books(
-        id int primary key, 
-        id_author int,
-        id_genre int,
-        title varchar,
-        page_number int)''')
-
-    cur.execute('''CREATE TABLE copy(
-        id int primary key, 
-        id_book int,
-        available boolean)''')
-
-    cur.execute('''CREATE TABLE borrows(
-         id int primary key, 
-         id_reader int REFERENCES readers(id),
-         id_copy int REFERENCES copy(id),
-         borrow_date date,
-         return_date date)''')
-
+    query = ("INSERT INTO " + arr[0] + arr[1] + " VALUES(" + str(values)[1:-1] + ")")
+    cur.execute(query)
     con.commit()
-
     con.close()
 
 def select():
     con = sqlite3.connect('library.db')
     cur = con.cursor()
-    data = cur.execute('select * from borrows').description
-    print(data)
+    data = cur.execute('select first_name, last_name, birth_date from authors').fetchall()
+    con.close()
+    return data
--- Library-System.py
+++ Library-System.py
@@ -1,5 +1,62 @@
 import Files.Database
+from tkinter import *
+from tkinter import ttk
+
+dictionary = {
+    "readers": ["readers", "(email, login, password, first_name, last_name, birth_date, city, street, house_number, phone)"],
+    "authors": ["authors", "(first_name, last_name, birth_date)"],
+    "genres": ["genres", "(name)"],
+    "books": ["books", "(id_author, id_genre, title, page_number)"],
+    "copy": ["copy", "(id_book, available)"],
+    "borrows": ["borrows", "(id_reader, id_copy, borrow_date, return_date)"]
+}
+
+
+
+
+
+x = Files.Database.select()
+
+root = Tk()
+root.title('Library')
+# root['bg'] = '#000'
+controlTab = ttk.Notebook(root)
+
+Authors = Frame(controlTab)
+Books = Frame(controlTab)
+
+authors_treeview = ttk.Treeview(Authors)
+
+authors_treeview['columns'] = ('first_name', 'last_name', 'birth_date')
+
+authors_treeview.column("#0", width=0,  stretch=NO)
+authors_treeview.column("first_name",anchor=CENTER, width=80)
+authors_treeview.column("last_name",anchor=CENTER,width=80)
+authors_treeview.column("birth_date",anchor=CENTER,width=80)
+
+authors_treeview.heading("#0",text="",anchor=CENTER)
+authors_treeview.heading("first_name",text="First Name",anchor=CENTER)
+authors_treeview.heading("last_name",text="Last Name",anchor=CENTER)
+authors_treeview.heading("birth_date",text="Birth Date",anchor=CENTER)
+
+
+for i in x:
+    authors_treeview.insert(parent='', index='end', iid=0, text='', values=i)
+
+authors_treeview.pack()
+
+controlTab.add(Authors, text='Authors')
+controlTab.add(Books, text='Books')
+controlTab.pack()
+
+
+root.mainloop()
+
+
+
+# Files.Database.insert(dictionary["authors"], ['Krzysztof', 'Krawczyk', '2022-01-01'])
+# Files.Database.insert(dictionary["readers"], ['ok@ok.pl', 'test', 'password', 'first', 'last', '2022-01-20', 'city', 'street', 'number', 'phone'])
 
 # Files.Database.create()
-
-Files.Database.select()
+#
+# Files.Database.select()
