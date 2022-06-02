3780fd4f31e4196d3dad3cd8b17bd82ac7872150
Karol Długoń <k.dlugon@appgo.pl>
2022-06-02 16:00:51 +0200

    insert authors (date)

--- Files/Insert/insert_authors.py
+++ Files/Insert/insert_authors.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkcalendar import DateEntry
 import Files.Database
 
 insert_authors = Tk()
@@ -8,20 +9,32 @@ insert_authors.configure(background="#bbb")
 
 Label(
     insert_authors,
-    text="First name",
-    bg='#CCCCCC',
-    ).grid(row=0, column=0, sticky=W, pady=10)
+    text="First name:",
+    bg='#bbb',
+    ).grid(row=0, column=0, sticky=W, padx=(20, 0), pady=(20, 0))
 
 Label(
     insert_authors,
-    text="Last name",
-    bg='#CCCCCC',
-    ).grid(row=1, column=0, sticky=W, pady=10)
+    text="Last name:",
+    bg='#bbb',
+    ).grid(row=1, column=0, sticky=W, padx=(20, 0), pady=(20, 0))
 
 Label(
     insert_authors,
-    text="Birth date",
-    bg='#CCCCCC',
-    ).grid(row=2, column=0, sticky=W, pady=10)
+    text="Birth date:",
+    bg='#bbb',
+    ).grid(row=2, column=0, sticky=W, padx=(20, 0), pady=(20, 0))
+
+
+Entry(insert_authors, width=40).grid(row=0, column=1, padx=(20, 20), pady=(20,0), ipadx=5, ipady=5)
+Entry(insert_authors, width=40).grid(row=1, column=1, padx=(20, 20), pady=(20,0), ipadx=5, ipady=5)
+Entry(insert_authors, width=40).grid(row=2, column=1, padx=(20, 20), pady=(20,0), ipadx=5, ipady=5)
+
+btn = ttk.Button(insert_authors, text="Add").grid(row=3, column=0, columnspan=2, sticky=W+E, padx=(20, 20), pady=(20, 20))
+
+
+cal = DateEntry(insert_authors, width=12, year=2019, month=6, day=22,
+background='darkblue', foreground='white', borderwidth=2).grid(row=4)
+
 
 insert_authors.mainloop()
